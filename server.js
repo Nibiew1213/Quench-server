a7f72b079497287acf3439896a709b2521eb019c
Mervin Poh <mervpoh@gmail.com>
2022-08-30 17:41:26 +0800

    initial: express boiler-plate

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require('express')
+const mongoose = require("mongoose");
+
+const app = express()
+const port = process.env.port || 3000
+
+const mongoConnectionStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@generalassembly.ljkj0.mongodb.net/?retryWrites=true&w=majority`;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(mongoConnectionStr, { dbName: "quencher" });
+  } catch (err) {
+    console.log(`Failed to connect to DB`);
+    process.exit(1);
+  }
+
+  console.log(`Example app listening on port ${port}`);
+});
