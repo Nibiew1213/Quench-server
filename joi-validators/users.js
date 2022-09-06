678eed4519545972d018420d1d9ff38eebce9fe9
Mervin Poh <mervpoh@gmail.com>
2022-09-06 14:49:47 +0800

    fix: users joi-validation

---
+++ joi-validators/users.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const validators = {
+    registerValidator: Joi.object({
+        fullName: Joi.string().min(3).max(140).label("Full Name").required(),
+        preferredName: Joi.string()
+          .min(3)
+          .max(60)
+          .label("Preferred Name")
+          .required(),
+        email: Joi.string()
+          .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+          .label("Email")
+          .required(),
+        password: Joi.string().min(3).label("Password").required(),
+        confirmPassword: Joi.any().valid(Joi.ref("password")).required().messages({
+          "any.only": '"Passwords" must match',
+        }),
+      }),
+}
+
+module.exports = validators
