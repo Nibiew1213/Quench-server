67d649b8add9582764780302afdd21ec0536f6fd
Mervin Poh <mervpoh@gmail.com>
2022-09-06 15:20:23 +0800

    fix: user reggo joi validation error handling

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,28 @@
 const userModel = require("../models/users-model");
 
 const userValidator = require("../joi-validators/users");
+const { beverageValidator } = require("../joi-validators/beverages");
 
 module.exports = {
     registerUser: (req, res) => {
-        res.send("user registered")
+        // res.send("user registered")
+
+        let errorObject = {}
+
+        //validate user reggo values
+        const userValidationResults = userValidator.registerValidator.validate(req.body, {
+            abortEarly: false,
+        })
+
+        if (userValidationResults.error) {
+            const validationError = userValidationResults.error.details
+
+            validationError.forEach((error) => {
+                errorObject[error.context.key] = error.message;
+            });
+
+            return res.status(400).json(errorObject);
+        }
+
     }
 };
