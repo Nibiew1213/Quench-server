66565359f1ad7bba234a7caf8a02187fad537bba
Wei Bin Wong <wongwb1990@gmail.com>
2022-09-16 08:17:16 +0800

    chore: changed dbName in seeds.js to match personal mongoDB database name for beverages file seeding

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Beverage = require('../models/beverages-model')
 const seedBeverage = require('./drinks/drinks.json')
 require("dotenv").config();
-const dbName = "quencher"
+const dbName = "Quench"
 
 mongoose
   .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}?retryWrites=true&w=majority`,{ dbName: dbName  } )
